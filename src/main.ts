9ee6b08eefb3c52620a2836a0b07da5b637a340b
Guilherme Faxina <gamerstarbr7@hotmail.com>
2022-12-13 19:03:11 -0300

    feat: url validation regex

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import database from './db/db.settings';
+import router from './routes/uri';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(router);
 
 app.listen(PORT, async () => {
   await database.sync();
